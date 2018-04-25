39ad40ebd8a0e9469492f7c30baa862335ba30b2
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-04-25 17:58:39 +0700

    vuejs workbox

---
+++ using-workbox-cli/injectManifest/my-project/sw-template.js
@@ -0,0 +1,20 @@
+// This is required line
+workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(
+  new RegExp('^https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst(),
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg|js|css|html)$/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
