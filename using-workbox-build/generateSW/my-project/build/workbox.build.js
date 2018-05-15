73dfafc642ad20f6aeb4a5e49d6df83baa678a25
Irfan Maulana <irfan.maulana@gdn-commerce.com>
2018-05-15 11:43:40 +0700

    Move vue.js to folders

--- using-workbox-build/generateSW/my-project/build/workbox.build.js
+++
@@ -1,9 +0,0 @@
-const { generateSW } = require('workbox-build');
-
-const swDest = 'dist/sw.js';
-generateSW({
-  swDest,
-  globDirectory: 'dist'
-}).then(({count, size}) => {
-  console.log(`Generated ${swDest}, which will precache ${count} files, totaling ${size} bytes.`);
-});
